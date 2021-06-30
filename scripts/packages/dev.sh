9d2ac66962fcc60cf9aac6aa321e7d6bc077a667
magicspon <magicspon@gmail.com>
2021-07-01 00:05:05 +0100

    fix: remove scripty

--- scripts/packages/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "┏━━━ 🧩 API REPORT: $(pwd) ━━━━━━━━━━━━━━━━━━━━━"
-yarn api-extractor run --local
--- scripts/packages/lint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "┏━━━ 🕵️‍♀️ LINT: eslint src --ext ts,js,tsx,jsx ━━━━━━━"
-yarn eslint src --ext ts,js,tsx,jsx
