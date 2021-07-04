a48effe74c3f8267498d52d5f2d06119533949f8
magicspon <magicspon@gmail.com>
2021-07-04 19:45:18 +0100

    feat: storybook ts alias, storybook svgs

--- packages/frontend/.storybook/main.js
+++ packages/frontend/.storybook/main.js
@@ -1,3 +1,8 @@
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
+const pathToInlineSvg = path.resolve(__dirname, '../src/svgs')
+
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
@@ -13,4 +18,25 @@ module.exports = {
 			},
 		},
 	],
+
+	webpackFinal: async (config) => {
+		const { rules } = config.module
+
+		config.resolve.plugins.push(
+			new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+		)
+
+		const fileLoaderRule = rules.find((rule) => rule.test?.test('.svg'))
+		fileLoaderRule.exclude = pathToInlineSvg
+
+		config.module.rules.push({
+			test: /\.svg$/,
+			include: pathToInlineSvg,
+			use: ['@svgr/webpack'],
+		})
+
+		config.resolve.alias['next/router'] = require.resolve('./router.mock.js')
+
+		return config
+	},
 }
--- packages/frontend/src/components/ui/Box/Box.tsx
+++ packages/frontend/src/components/ui/Box/Box.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
+import Icon from '@svgs/delete.svg'
 
 interface IBox {
 	children: React.ReactNode
 }
 
 export default function IBox({ children }: IBox): JSX.Element {
-	return <div className="bg-blue-500 text-white">{children}</div>
+	return (
+		<div className="text-white bg-blue-500">
+			{children} <Icon />
+		</div>
+	)
 }
